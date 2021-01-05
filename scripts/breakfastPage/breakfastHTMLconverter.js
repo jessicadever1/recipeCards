5869311ab769af096cd49ca90140810b87472e3c
jessicadever1 <jessicadever1@gmail.com>
2021-01-04 21:17:28 -0600

    added calories and servings to form, got the recipes to populate on correct pages, still not finished with form

--- scripts/breakfastPage/breakfastHTMLconverter.js
+++ scripts/breakfastPage/breakfastHTMLconverter.js
@@ -4,7 +4,7 @@ export const breakfastRecipeHTMLConverter = (recipe) => {
             
                 <img class="recipeMainImg" src="../images/${recipe.imgName}.JPG" alt="recipe picture">
                 <div class="recipeCardContent">
-                    <h2 class="recipeName" id="recipeName">${recipe.recipeName}</h2>
+                    <h2 class="breakfastRecipeName" id="recipeName">${recipe.recipeName}</h2>
                     <p class="calorieCount" id="calorieCount">${recipe.totalCalories} Calories</p>
                     <p class="servings" id="servings">Servings: ${recipe.servings}</p>
                     <p class="totalTime">Total Time: ${recipe.totalTime}</p>
--- scripts/form.js
+++ scripts/form.js
@@ -15,17 +15,26 @@ const renderForm = () => {
                                     <input type="text" id="recipeName" class="recipeNameInput"></input>
                                 </div>
                                 <div class="recipeTypeDiv">
-                                <label for="mealSelect"><h3 class="mealSelectTitle">Select a meal</h3>
-                                <select class="mealSelect" id="mealSelect">
-                                    <option value="0" class="select">  Please select a meal...</option>
-                                    <option value="1" id="breakfastSelect">Breakfast</option>
-                                    <option value="2" class="lunchSelect">Lunch</option>
-                                    <option value="3" class="snackSelect">Snack</option>
-                                    <option value="4" class="dinnerSelect">Dinner</option>
-                                    <option value="5" class="dessertSelect">Dessert</option> 
-                                </select>
+                                    <label for="mealSelect"><h3 class="mealSelectTitle">Select a meal</h3>
+                                    <select class="mealSelect" id="mealSelect">
+                                        <option value="0" class="select">  Please select a meal...</option>
+                                        <option value="Breakfast" id="breakfastSelect">Breakfast</option>
+                                        <option value="Lunch" class="lunchSelect">Lunch</option>
+                                        <option value="Snack" class="snackSelect">Snack</option>
+                                        <option value="Dinner" class="dinnerSelect">Dinner</option>
+                                        <option value="Dessert" class="dessertSelect">Dessert</option> 
+                                    </select>
                                 </div>
-                                    
+                                <div class="calAndServeInput">
+                                    <section class="caloriesDiv">
+                                        <label for="caloriesInput" class="calAndServe">Calories</label>
+                                        <input type="text" id="caloriesInput" class="caloriesInput" placeholder="Calories"></input>
+                                    </section>
+                                    <div class="servingsDiv">
+                                        <label for="servingsInput" class="calAndServe">Servings</label>
+                                        <input type="text" id="servingsInput" class="servingsInput" placeholder="Servings">
+                                    </div>
+                                </div>    
                             </div>    
                             <div class="topRightOfRecipeForm">
                                     <div class="iconSelect">
@@ -53,30 +62,30 @@ const renderForm = () => {
                             <div class="ingredientInputs">
                                 <select class="recipeIngredients__quantity" type="text" placeholder="quantity" id="recipeIngredientsQuantity">
                                     <option value="0">Quantity<option>
-                                    <option value="2">&#8539;<option>
-                                    <option value="3">&#188;<option>
-                                    <option value="4">&#8531;<option>
-                                    <option value="5">&#189;<option>
-                                    <option value="6">&#8532;<option>
-                                    <option value="7">&#190;<option>
-                                    <option value="8">1<option>
-                                    <option value="10">1 &#8539;<option>
-                                    <option value="11">1 &#188;<option>
-                                    <option value="12">1 &#8531;<option>
-                                    <option value="13">1 &#189;<option>
-                                    <option value="14">1 &#8532;<option>
-                                    <option value="15">1 &#190;<option>
-                                    <option value="16">2<option>
+                                    <option value="&#8539;">&#8539;<option>
+                                    <option value="&#188;">&#188;<option>
+                                    <option value="&#8531;">&#8531;<option>
+                                    <option value="&#189;">&#189;<option>
+                                    <option value="&#8532;">&#8532;<option>
+                                    <option value="&#190;">&#190;<option>
+                                    <option value="1">1<option>
+                                    <option value="1 &#8539;">1 &#8539;<option>
+                                    <option value="1 &#188;">1 &#188;<option>
+                                    <option value="1 &#8531;">1 &#8531;<option>
+                                    <option value="1 &#189;">1 &#189;<option>
+                                    <option value="1 &#8532;">1 &#8532;<option>
+                                    <option value="1 &#190;">1 &#190;<option>
+                                    <option value="2">2<option>
                             </select>
                             <select id="unitSelected" class="recipeIngredients__unit" type="text" placeholder="Unit">Unit
                                     <option value="0">Unit</option>
-                                    <option value="1">teaspoon</option>
-                                    <option value="2">tablespoon</option>
-                                    <option value="3">cup</option>
-                                    <option value="4">oz</option>
-                                    <option value="5">pint</option>
-                                    <option value="6">quart</option>
-                                    <option value="7">gallon</option>
+                                    <option value="Teaspoon">Teaspoon</option>
+                                    <option value="Tablespoon">Tablespoon</option>
+                                    <option value="Cup">Cup</option>
+                                    <option value="oz">oz</option>
+                                    <option value="Pint">Pint</option>
+                                    <option value="Quart">Quart</option>
+                                    <option value="Gallon">Gallon</option>
                             </select>
                                 <input id="ingredientInput" class="recipeIngredients__ingredient" type="text" placeholder="Ingredient Name">
                                 <input id="ingredientBrand" class="recipeIngredients__brand" type="text" placeholder="Brand">
@@ -101,13 +110,15 @@ export const recipeFormComponent = () => {
 
 eventHub.addEventListener("click", (clickEvent) => {
     if(clickEvent.target.id === "selectRecipeButton") {
+        const imgName = "temp"
         const recipeName = document.querySelector("#recipeName").value
         const mealType = document.querySelector("#mealSelect").value
         // const vegan = document.querySelector(".").value
         // const vegetarian = document.querySelector(".").value
         // const whole30 = document.querySelector(".").value
         // const fiveHundCalories = document.querySelector(".").value
-        // const servings = document.querySelector(".").value
+        const servings = document.querySelector("#servingsInput").value
+        const calories = document.querySelector("#caloriesInput").value
         // const prepTime = document.querySelector(".").value
         // const cookTime = document.querySelector(".").value
         // const totalTime = document.querySelector(".").value
@@ -119,13 +130,15 @@ eventHub.addEventListener("click", (clickEvent) => {
         const instructions = document.querySelector("#instruction").value
     
         const newRecipe = {
+            imgName: imgName,
             recipeName: recipeName,
             mealType: mealType,
             // vegan: vegan,
             // vegetarian: vegetarian,
             // whole30: whole30,
             // fiveHundCalories: fiveHundCalories,
-            // servings: servings,
+            calories: calories,
+            servings: servings,
             // prepTime: prepTime,
             // cookTime: cookTime,
             // totalTime: totalTime,
